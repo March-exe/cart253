a62417a7df6ef67233258b397ea7f61880a55670
March-exe <marcianofaugno@hotmail.com>
2026-09-21 17:56:49 -0400

    movie countdown - working attempt 1

--- topics/assignments/Prototypes/Countdown/js/script.js
+++ topics/assignments/Prototypes/Countdown/js/script.js
@@ -10,58 +10,53 @@
 
 
 
-// Click and drag the mouse to view the scene from different angles.
+// function calls for set up
 
 function setup() {
+    //creates a 400x400 canvas
     createCanvas(400, 400);
 
+    // changes the angle mode to degrees instead of radian
     angleMode(DEGREES);
-    frameRate(60)
+    //set frame rate to 60fps
+    frameRate(60);
+    //sets variables for counter and arc animation
+    textNum = 9
+    stopAngle = -90
+
 }
 
 function draw() {
     background(255, 200, 200);
 
-
     line(200, 400, 200, 0);
     line(0, 200, 400, 200);
-
-
 
     // Style the arc.
     push();
     noStroke();
     fill(200, 150, 255);
-    // Updates stop angles.
-    let stopAngle = -90 + (frameCount * 6);
+    // Updates stop angles
+    if (textNum > 0)
+        //Sets the stopping angle to -90, adds 60/6 degrees creating animation
+        stopAngle = -90 + frameCount * 6;
 
     // Draw the arc.
-    arc(200, 200, 180, 180, -90, stopAngle);
     //arc(x, y, w, h, startAngle, endangle, [mode], [detail])
+    arc(200, 200, 180, 180, -90, stopAngle);
     pop();
 
 
-    let textNum = 9
-    let number = 0
+    // if the displayed number is greater than zero and the frameCount's remainer is 0, then the displayed number will decrease by 1
+    if (textNum > 0 && frameCount % 60 == 0)
+        textNum -= 1;
 
-
-
-    //while (number < 9)
-    if (frameCount / 60)
-        number = number + (frameCount / 60);
-    textNum = textNum - number;
-
-
-
-
-    //updates numbers for amount of rotations a second
     rectMode(CENTER);
-    textSize(100)
+    textSize(100);
     text(textNum, 170, 230);
 
-    //testing number for counter
-    text(number, 100, 100);
 
-
+    //DEBUGGING TOOL: updates number for every second past based on framerate
+    //text(frameCount/60 , 100, 100)
 
 }
